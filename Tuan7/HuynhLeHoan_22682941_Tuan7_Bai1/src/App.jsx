b2dc47dbe4aa57c21600c4ddbdd260d366c58cbc
Huynhlehoan <22682941.hoan@student.iuh.edu.vn>
2025-04-06 14:27:39 +0700

    7. SPA -Single Application Page lan1

--- Tuan7/HuynhLeHoan_22682941_Tuan7_Bai1/src/App.jsx
+++ Tuan7/HuynhLeHoan_22682941_Tuan7_Bai1/src/App.jsx
@@ -1,46 +1,29 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Sidebar from './components/Sidebar';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Dashboard from './pages/DashBoard';
 import Project from './pages/Project';
 import Teams from './pages/Teams';
 import Analytics from './pages/Analytics';
 import Messages from './pages/Messages';
 import Integrations from './pages/Integrations';
-import Overview from './components/OverviewCard';
+import RootLayout from './layout/RootLayout'; 
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { path: '', element: <Dashboard /> },
+      { path: 'project', element: <Project /> },
+      { path: 'teams', element: <Teams /> },
+      { path: 'analytics', element: <Analytics /> },
+      { path: 'messages', element: <Messages /> },
+      { path: 'integrations', element: <Integrations /> }
+    ]
+  }
+]);
 
 function App() {
-  return (
-    <Router>
-      <div className="container min-h-screen">
-        <div className="header">
-          <Header />
-        </div>
-
-        <div className="menu">
-          <Sidebar />
-        </div>
-
-  
-        <div className="content space-y-4">
-          <Overview />
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/project" element={<Project />} />
-            <Route path="/teams" element={<Teams />} />
-            <Route path="/analytics" element={<Analytics />} />
-            <Route path="/messages" element={<Messages />} />
-            <Route path="/integrations" element={<Integrations />} />
-          </Routes>
-        </div>
-
-
-        {/* <div className="footer">
-          <Footer />
-        </div> */}
-      </div>
-    </Router>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
